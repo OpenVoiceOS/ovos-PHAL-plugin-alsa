e43a646ebacbb6881c1c9b1e624c2793f143a5f3
Daniel McKnight <daniel@neon.ai>
2022-09-08 11:12:48 -0700

    Update package name copy/paste errors

--- setup.py
+++ setup.py
@@ -3,6 +3,33 @@ import os
 
 from setuptools import setup
 
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join(BASEDIR, 'ovos_PHAL_plugin_alsa', 'version.py')
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha and int(alpha) > 0:
+        version += f"a{alpha}"
+    return version
+
 
 def package_files(directory):
     paths = []
@@ -12,10 +39,20 @@ def package_files(directory):
     return paths
 
 
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        if 'MYCROFT_LOOSE_REQUIREMENTS' in os.environ:
+            print('USING LOOSE REQUIREMENTS!')
+            requirements = [r.replace('==', '>=').replace('~=', '>=') for r in requirements]
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
+
 PLUGIN_ENTRY_POINT = 'ovos-PHAL-plugin-alsa=ovos_PHAL_plugin_alsa:AlsaVolumeControlPlugin'
 setup(
     name='ovos-PHAL-plugin-alsa',
-    version='0.0.2',
+    version=get_version(),
     description='A volume control plugin for OpenVoiceOS hardware abstraction layer',
     url='https://github.com/OpenVoiceOS/ovos-PHAL-plugin-alsa',
     author='JarbasAi',
@@ -23,10 +60,7 @@ setup(
     license='Apache-2.0',
     packages=['ovos_PHAL_plugin_alsa'],
     package_data={'': package_files('ovos_PHAL_plugin_alsa')},
-    install_requires=[
-        "ovos-plugin-manager~=0.0",
-        "json_database~=0.7",
-        "pyalsaaudio~=0.9"],
+    install_requires=required("requirements.txt"),
     zip_safe=True,
     include_package_data=True,
     classifiers=[
